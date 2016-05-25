011c96d9d4a03011a7f3717879fda287535940e7
Yarossia <gtibmx11@gmail.com>
2016-05-26 02:38:23 +0300

    Improved notifier for styles task

--- tasks/styles.js
+++ tasks/styles.js
@@ -5,6 +5,7 @@ import autoprefix from 'gulp-autoprefixer'
 import sassLint from 'gulp-sass-lint';
 import minify from 'gulp-minify-css';
 import rename from 'gulp-rename';
+import notify from 'gulp-notify';
 // import sourcemaps from 'gulp-sourcemaps'; - because current version do not support node 6.1.0
 
 const src = {
@@ -14,7 +15,11 @@ gulp.task('styles:build', () => (
         gulp.src(src.sassStyles)
             .pipe(plumber())
           //  .pipe(sourcemaps.init())
-            .pipe(sass().on('error', sass.logError))
+            .pipe(sass()
+            .on("error", notify.onError({
+                message: "Error: <%= error.message %>",
+                title: "Error compiling sass" 
+            })))
             .pipe(autoprefix({
                 browsers: ['last 2 versions'],
                 cascade: false
@@ -24,13 +29,18 @@ gulp.task('styles:build', () => (
           //  .pipe(sourcemaps.write())
             .pipe(plumber.stop())
             .pipe(gulp.dest('core/assets/styles'))
+            .pipe(notify("SASS has been compiled."))
 ));
 
 gulp.task('styles:lint', () => (
         gulp.src(src.sassStyles)
             .pipe(sassLint())
             .pipe(sassLint.format())
-            .pipe(sassLint.failOnError())
+            .pipe(sassLint.failOnError()
+            .on("error", notify.onError({
+                message: "Error: <%= error.message %>",
+                title: "Error linting sass"
+            })))
 ));
 
 gulp.task('styles', ['styles:lint', 'styles:build']);
